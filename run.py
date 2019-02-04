df2a9cab9710574a5051d0f6a3576abe140b2c31
dunyung1 <dshege4@gmail.com>
2019-02-04 15:06:10 +0300

    change my movie api to display

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from app import app
 
-if __name__ == '__main__':    # chekcing if the script is run directly
+if __name__ == '__main__':    # checking if the script is run directly
     app.run()   #used to lauch our server.
